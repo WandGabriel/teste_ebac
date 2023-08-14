b222e5a4cc0998e6e9effcced2d0f300dab07a3c
Wanderson Gabriel <gabriel_wanderson@hotmail.com>
2023-08-14 18:05:04 -0300

    Melhoria na lógica de validação alterando String para Number e estilização dos campos finais de validação.

--- exercicio_js/main.js
+++ exercicio_js/main.js
@@ -1,11 +1,12 @@
 const formulario = document.getElementById('form-edit');
 
 function validaValores(num1, num2){    
-    console.log("Primeiro Número: " + num1);
-    console.log("Segundo Número:" + num2);
 
-    console.log("Número: "+num1+ " > Número: "+num2);
-    console.log(num1 >num2)
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+
+    return num1 > num2;
+
 }
 
 console.log(formulario);
@@ -15,5 +16,29 @@ formulario.addEventListener('submit', function(e){
 
     const primeiroNumero = document.getElementById ('first-number');
     const segundoNumero = document.getElementById ('second-number');
-    validaValores(primeiroNumero.value, segundoNumero.value);
+    const resultadovalidacao = validaValores(primeiroNumero.value, segundoNumero.value);
+    const mensagemRetornoTrue = `<b>Parabéns o número ${primeiroNumero.value} é maior!<b>`
+    const mensagemRetornoFalse = `<b>Refaça o teste novamente o ${primeiroNumero.value} não é maior!</b>`
+
+    if(resultadovalidacao){
+        const mensagemSucesso = document.querySelector('.success-validation')
+        mensagemSucesso.innerHTML  = mensagemRetornoTrue;
+        mensagemSucesso.style.display = 'block';
+        mensagemSucesso.style.fontSize= '20px';
+        mensagemSucesso.style.background = 'green'
+
+        primeiroNumero.value = '';
+        segundoNumero.value = '';
+    } else {
+        const mensagemSucesso = document.querySelector('.success-validation')
+        mensagemSucesso.innerHTML = mensagemRetornoFalse;
+        mensagemSucesso.style.display = 'block';
+        mensagemSucesso.style.background = 'red';
+        mensagemSucesso.style.fontSize= '20px';
+
+        primeiroNumero.value = '';
+        segundoNumero.value = '';
+
+        document.querySelector('.success-validation').innerHTML = mensagemRetornoFalse;
+    }
 })
