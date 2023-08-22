1dc2f4ae379f0cb93d2dfa678e2a57f7758269e3
Wanderson Gabriel <gabriel_wanderson@hotmail.com>
2023-08-21 21:20:28 -0300

    Inserção de Lógica JS para riscar tarefas.

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
     console.log($('#create-task'));
 })
+
+$('form').on('submit',function(e){
+    e.preventDefault();
+    
+    const taskUser = $('#text-user-form').val();
+    const novaDiv = $('<div class="items"></div>');
+    const novoItem = $('<li></li>').appendTo(novaDiv);
+    $(`<p>${taskUser}</p>`).appendTo(novoItem);
+    $(novaDiv).appendTo('ul');
+    $('#text-user-form').val('');
+})
+
+$('div.items').on('click',function(){
+    $(this).toggleClass("itemsSelected");
+})
